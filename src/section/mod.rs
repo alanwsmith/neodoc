8928a075fea3746872e735ab74ef8b6a4682d5e2
Alan Smith <commit@alanwsmith.com>
2026-07-30 15:57:35 -0400

    moved p out to its own file

--- src/section/mod.rs
+++ src/section/mod.rs
@@ -1,13 +1,10 @@
 pub mod block;
 pub mod p;
-use crate::bound::*;
 use crate::metadata::*;
-use crate::parsers::*;
-use nom::character::complete::line_ending;
 use nom::{IResult, Parser};
 
 pub use block::*;
-// pub use p::*;
+pub use p::*;
 
 use crate::span::*;
 use serde::{Deserialize, Serialize};
@@ -25,36 +22,6 @@ pub enum Section {
 }
 
 pub fn section(input: &str) -> IResult<&str, Section> {
-  let (input, _) = section_token.parse(input)?;
-  let (input, r#type) = section_type.parse(input)?;
-  let (input, kind) = section_kind.parse(input)?;
-  let (input, _) = line_ending.parse(input)?;
-  let (input, _) = empty_lines.parse(input)?;
-  let (input, block) = block.parse(input)?;
-  let section = match kind {
-    Some(_k) => Section::P {
-      metadata: {
-        Metadata {
-          attrs: vec![],
-          bound: Bound::Full,
-          flags: vec![],
-          r#type: r#type.to_string(),
-        }
-      },
-      sections: vec![block],
-    },
-    None => Section::P {
-      metadata: {
-        Metadata {
-          attrs: vec![],
-          bound: Bound::Full,
-          flags: vec![],
-          r#type: r#type.to_string(),
-        }
-      },
-      sections: vec![block],
-    },
-  };
-
+  let (input, section) = p.parse(input)?;
   Ok((input, section))
 }
---
+++ src/section/p.rs
@@ -0,0 +1,42 @@
+use crate::bound::*;
+use crate::metadata::*;
+use crate::parsers::*;
+use crate::section::Section;
+use crate::section::*;
+use nom::character::complete::line_ending;
+use nom::{IResult, Parser};
+
+pub fn p(input: &str) -> IResult<&str, Section> {
+  let (input, _) = section_token.parse(input)?;
+  let (input, r#type) = section_type.parse(input)?;
+  let (input, kind) = section_kind.parse(input)?;
+  let (input, _) = line_ending.parse(input)?;
+  let (input, _) = empty_lines.parse(input)?;
+  let (input, block) = block.parse(input)?;
+  let section = match kind {
+    Some(_k) => Section::P {
+      metadata: {
+        Metadata {
+          attrs: vec![],
+          bound: Bound::Full,
+          flags: vec![],
+          r#type: r#type.to_string(),
+        }
+      },
+      sections: vec![block],
+    },
+    None => Section::P {
+      metadata: {
+        Metadata {
+          attrs: vec![],
+          bound: Bound::Full,
+          flags: vec![],
+          r#type: r#type.to_string(),
+        }
+      },
+      sections: vec![block],
+    },
+  };
+
+  Ok((input, section))
+}
