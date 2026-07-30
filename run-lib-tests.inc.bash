a6e7256a38a61c685d4059071a51e6bfad02756c
Alan Smith <commit@alanwsmith.com>
2026-07-29 23:40:54 -0400

    added clippy to test watcher

--- run-lib-tests.inc.bash
+++ run-lib-tests.inc.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-cargo watch -c -x "test -- --nocapture"
+cargo watch -c -x "test -- --nocapture && cargo clippy"
+
+# cargo watch -q -c -x 'test solo -- --nocapture --quiet && cargo test -- --nocapture --quiet && cargo clippy --quiet && cargo run --quiet'
 
