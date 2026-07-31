4cdec90ff4837226c661b96374aaa0840945777f
Alan Smith <commit@alanwsmith.com>
2026-07-31 09:07:54 -0400

    geting ready to move to single test file

--- tests/attr.rs
+++ tests/attr.rs
@@ -9,7 +9,7 @@ use std::path::Path;
 #[allow(dead_code)]
 struct AttrTest {
   key: String,
-  skip: bool,
+  skip: Option<bool>,
   given: String,
   status: Status,
   remainder: String,
@@ -25,31 +25,33 @@ enum Status {
 fn my_test(path: &Path) -> datatest_stable::Result<()> {
   let content = &fs::read_to_string(path)?;
   let test: AttrTest = serde_json::from_str(content)?;
-  match test.status {
-    Status::Ok(data) => {
-      let left = (test.remainder.as_str(), data);
-      match test.key.as_str() {
-        "multi_line_attr" => {
-          let result = attr(&test.given).unwrap();
-          let right = (
-            result.0,
-            serde_json::to_value(result.1).unwrap(),
-          );
-          assert_eq!(left, right);
-        }
-        "single_line_attr" => {
-          let result = attr(&test.given).unwrap();
-          let right = (
-            result.0,
-            serde_json::to_value(result.1).unwrap(),
-          );
-          assert_eq!(left, right);
-        }
-        _ => (),
-      };
-    }
-    Status::Error(_data) => {
-      panic!("set up for errors")
+  if test.skip.is_none() {
+    match test.status {
+      Status::Ok(data) => {
+        let left = (test.remainder.as_str(), data);
+        match test.key.as_str() {
+          "multi_line_attr" => {
+            let result = attr(&test.given).unwrap();
+            let right = (
+              result.0,
+              serde_json::to_value(result.1).unwrap(),
+            );
+            assert_eq!(left, right);
+          }
+          "single_line_attr" => {
+            let result = attr(&test.given).unwrap();
+            let right = (
+              result.0,
+              serde_json::to_value(result.1).unwrap(),
+            );
+            assert_eq!(left, right);
+          }
+          _ => (),
+        };
+      }
+      Status::Error(_data) => {
+        panic!("set up for errors")
+      }
     }
   }
   Ok(())
