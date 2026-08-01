b9a41469bd650030fc113835aadac429b6ad8403
Alan Smith <commit@alanwsmith.com>
2026-08-01 08:43:25 -0400

    moved to camelCase functions

--- sed_replace.inc.bash
+++ sed_replace.inc.bash
@@ -7,6 +7,6 @@
 
 find "/Users/alan/workshop/neodoc/content/" \
   -type f \( -iname "*.js" -o -iname "*.html" -o -iname "*.txt" -o -iname "*.md" -o -iname "*.neo" \) -print0 | xargs -0 sed -E -i "" \
-  's|template_dir|dir|g'
+  's|file_list|fileList|g'
 
 
