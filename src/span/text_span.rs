735783a72ce9d90035d490711ae1a5ead593bdae
Alan Smith <commit@alanwsmith.com>
2026-07-31 22:36:26 -0400

    using whitespace1 for text_span

--- src/span/text_span.rs
+++ src/span/text_span.rs
@@ -1,13 +1,13 @@
 use crate::span::single_newline::single_newline;
+use crate::span::whitespace1::whitespace1;
 use crate::span::word::word;
 use nom::branch::alt;
-use nom::character::complete::space1;
 use nom::multi::many1;
 use nom::{IResult, Parser};
 
 pub fn text_span(input: &str) -> IResult<&str, String> {
   let (input, results) =
-    many1(alt((word, single_newline, space1)))
+    many1(alt((word, single_newline, whitespace1)))
       .parse(input)?;
   Ok((input, results.join("")))
 }
@@ -21,7 +21,7 @@ mod tests {
   #[rstest]
   #[case("alfa bravo", "alfa bravo".to_string(), "")]
   #[case("alfa bravo\ncharlie delta", "alfa bravo charlie delta".to_string(), "")]
-  #[case("alfa bravo  \n  charlie delta", "alfa bravo charlie delta".to_string(), "")]
+  #[case("alfa bravo   \n   charlie delta", "alfa bravo charlie delta".to_string(), "")]
   #[case("alfa\nbravo\n\ncharlie delta", "alfa bravo".to_string(), "\n\ncharlie delta")]
   #[case("alfa      bravo", "alfa bravo".to_string(), "")]
   fn text_span_runner(
