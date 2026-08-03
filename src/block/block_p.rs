007d35498c98b1ea5605f713fca092f79d42d6fd
Alan Smith <commit@alanwsmith.com>
2026-08-03 09:17:12 -0400

    put block_p tests back in place

--- src/block/block_p.rs
+++ src/block/block_p.rs
@@ -5,28 +5,19 @@ use crate::section::*;
 use crate::span::Span;
 use crate::span::empty_lines_or_eof::empty_lines_or_eof;
 use crate::span::text_span::text_span;
-use nom::character::complete::line_ending;
 use nom::multi::many1;
 use nom::{IResult, Parser};
 
 pub fn block_p(mut input: Text) -> IResult<Text, Section> {
   input.extra = "block_p";
-  dbg!("aaaaaaaaaaaaaaaaaaaaaaa");
-  dbg!(&input);
   let metadata = Metadata {
     attrs: vec![],
     bound: Bound::Full,
     flags: vec![],
     r#type: "block".to_string(),
   };
-  dbg!("bbbbbbbbbbbbbbbbbbbbbbbbbbb");
-  dbg!(&input);
   let (input, span_strs) = many1(text_span).parse(input)?;
-  dbg!("ccccccccccccccccccccccccccccccc");
-  dbg!(&input);
   let (input, _) = empty_lines_or_eof.parse(input)?;
-  dbg!("dddddddddddddddddddddddddddd");
-  dbg!(&input);
   let spans = span_strs
     .iter()
     .map(|x| Span::Text {
@@ -44,12 +35,12 @@ mod tests {
 
   #[test]
   fn block_p_basic() {
-    let input = Text::new_extra("alfa", "");
+    let content = "alfa";
+    let target =
+      r#"[ { "kind": "text", "content": "alfa" }]"#;
+    let input = Text::new_extra(content, "");
     let result = block_p.parse(input).unwrap().1;
-    let left: Value = serde_json::from_str(
-      r#"[ { "kind": "text", "content": "alfa" }]"#,
-    )
-    .unwrap();
+    let left: Value = serde_json::from_str(target).unwrap();
     if let Section::PBlock { ref spans, .. } = result {
       assert_eq!(
         left,
@@ -62,31 +53,41 @@ mod tests {
     }
   }
 
-  // #[test]
-  // fn block_p_multiple_lines() {
-  //   let input = "alfa bravo\ncharlie delta";
-  //   let result = block_p.parse(input).unwrap().1;
-  //   let left: Value = serde_json::from_str(
-  //     r#"[ { "kind": "text", "content": "alfa bravo charlie delta" }]"#,
-  //   )
-  //   .unwrap();
-  //   if let Section::PBlock { spans, .. } = result {
-  //     assert_eq!(
-  //       left,
-  //       serde_json::to_value(spans).unwrap()
-  //     );
-  //   } else {
-  //     panic!("Failed to get result");
-  //   }
-  // }
+  #[test]
+  fn block_p_multi_line() {
+    let content = "alfa bravo\ncharlie delta";
+    let target = r#"[ { "kind": "text", "content": "alfa bravo charlie delta" }]"#;
+    let input = Text::new_extra(content, "");
+    let result = block_p.parse(input).unwrap().1;
+    let left: Value = serde_json::from_str(target).unwrap();
+    if let Section::PBlock { ref spans, .. } = result {
+      assert_eq!(
+        left,
+        serde_json::to_value(spans).unwrap(),
+        "{}",
+        format!("\n\n{:?}\n\n{:?}", input, result)
+      );
+    } else {
+      panic!("Failed to get result");
+    }
+  }
 
-  // #[test]
-  // fn block_p_multiple_lines_followed_by_empty_line() {
-  //   let input = "alfa bravo\ncharlie delta\n\nx";
-  //   let right = block_p.parse(input).unwrap().0;
-  //   let left = "x";
-  //   assert_eq!(left, right);
-  // }
-
-  //
+  #[test]
+  fn block_p_multi_line_followed_by_empty_line() {
+    let content = "alfa bravo\ncharlie delta\n\nx";
+    let target = r#"[ { "kind": "text", "content": "alfa bravo charlie delta" }]"#;
+    let input = Text::new_extra(content, "");
+    let result = block_p.parse(input).unwrap().1;
+    let left: Value = serde_json::from_str(target).unwrap();
+    if let Section::PBlock { ref spans, .. } = result {
+      assert_eq!(
+        left,
+        serde_json::to_value(spans).unwrap(),
+        "{}",
+        format!("\n\n{:?}\n\n{:?}", input, result)
+      );
+    } else {
+      panic!("Failed to get result");
+    }
+  }
 }
