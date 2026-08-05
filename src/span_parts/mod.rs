fce46497dac9078474cb67649e27db536fdddc41
Alan Smith <commit@alanwsmith.com>
2026-08-05 14:51:36 -0400

    added single newline in metadata

--- src/span_parts/mod.rs
+++ src/span_parts/mod.rs
@@ -1,2 +1,3 @@
 pub mod one_or_more_colons;
 pub mod one_or_more_dashes;
+pub mod single_newline_in_metadata;
---
+++ src/span_parts/single_newline_in_metadata.rs
@@ -0,0 +1,110 @@
+use crate::Text;
+use nom::bytes::complete::tag;
+use nom::character::complete::{line_ending, space0};
+use nom::combinator::{eof, not, opt};
+use nom::{IResult, Parser};
+
+// REMINDER: this works the same was as
+// single_newline but stops if the following
+// line has dashes which is what lets it
+// be used in section metadata.
+
+pub fn single_newline_in_metadata(
+  input: Text
+) -> IResult<Text, Text> {
+  let (input, _) = space0.parse(input)?;
+  let (input, _) = line_ending.parse(input)?;
+  if input.fragment().is_empty() {
+    return Ok((input, Text::new_extra("", "")));
+  }
+  let (_, check) = opt((space0, eof)).parse(input)?;
+  if check.is_some() {
+    return Ok((input, Text::new_extra("", "")));
+  }
+  let (input, _) = space0.parse(input)?;
+  let (input, _) = not(tag("-")).parse(input)?;
+  let (input, _) =
+    not((space0, line_ending)).parse(input)?;
+  Ok((input, Text::new_extra(" ", "")))
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use pretty_assertions::assert_eq;
+  use rstest::rstest;
+
+  #[rstest]
+  #[case(
+    "single newline at end of file returns empty string",
+    "\n",
+    "",
+    ""
+  )]
+  #[case(
+    "single newline with content after it returns space",
+    "\nx",
+    " ",
+    "x"
+  )]
+  #[case(
+    "single newline with spaces then content after it returns space and chomps whitespace on the next line",
+    "\n  x",
+    " ",
+    "x"
+  )]
+  #[case(
+    "single newline followed by line with only whitespace that reaches end of file returns an empty string",
+    "\n ",
+    "",
+    " "
+  )]
+
+  fn single_newline_in_metadata_test_runner(
+    #[case] description: &str,
+    #[case] given: &str,
+    #[case] expected: &str,
+    #[case] remainder: &str,
+  ) {
+    let input = Text::new_extra(given, "");
+    let result =
+      single_newline_in_metadata.parse(input).unwrap();
+    assert_eq!(
+      &expected,
+      result.1.fragment(),
+      "\n\n{}\n\n",
+      description
+    );
+    assert_eq!(
+      &remainder,
+      result.0.fragment(),
+      "\n\n{}\n\n",
+      description
+    );
+  }
+
+  #[rstest]
+  #[case("multiple newlines are an error", "\n\n")]
+  #[case(
+    "multiple followed by empty line with whitespace is an error",
+    "\n     \n"
+  )]
+  #[case(
+    "single newline followed by a dash is an error",
+    "\n-"
+  )]
+  fn single_newline_in_metadata_error_test_runner(
+    #[case] description: &str,
+    #[case] given: &str,
+  ) {
+    let input = Text::new_extra(given, "");
+    let result = single_newline_in_metadata.parse(input);
+    assert!(
+      result.is_err(),
+      "\n\nFAILED: {}\n\n",
+      description
+    );
+  }
+
+  //
+}
