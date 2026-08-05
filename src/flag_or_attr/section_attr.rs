8d5e95c44a85f7c1841f4b257c8665c23690cfc5
Alan Smith <commit@alanwsmith.com>
2026-08-05 14:28:51 -0400

    making sure single_newline works at end of file

--- src/flag_or_attr/section_attr.rs
+++ src/flag_or_attr/section_attr.rs
@@ -2,11 +2,6 @@ use crate::Text;
 use crate::flag_or_attr::FlagOrAttr;
 use crate::span::attribute_text_span::attribute_text_span;
 use crate::span::section_token::section_token;
-use crate::span::single_character::single_colon;
-use crate::span::single_newline::single_newline;
-use crate::span::text_span::*;
-use crate::span::{Span, word_part::word_part};
-use nom::branch::alt;
 use nom::bytes::complete::{is_not, tag};
 use nom::character::complete::{line_ending, space1};
 use nom::combinator::opt;
@@ -55,13 +50,14 @@ pub fn section_attr(
 #[cfg(test)]
 mod tests {
   use super::*;
+  use crate::span::Span;
   use pretty_assertions::assert_eq;
   use rstest::rstest;
   use serde_json::Value;
 
   #[rstest]
   #[case(
-    "key: words",
+    "key, word, eof",
     "-- alfa: bravo",
     "alfa",
     r#"[
@@ -76,6 +72,23 @@ mod tests {
     ]"#,
     ""
   )]
+  #[case(
+    "key, word, newline",
+    "-- alfa: bravo\n",
+    "alfa",
+    r#"[
+    {
+      "attributes": [], 
+      "content": "bravo", 
+      "flags": [],
+      "kind": "span", 
+      "name": "text", 
+      "template": "default"
+    }
+    ]"#,
+    ""
+  )]
+
   fn section_attribute_runner(
     #[case] description: &str,
     #[case] given: &str,
@@ -102,28 +115,6 @@ mod tests {
       "\n\n{}\n\n",
       description,
     );
-  }
-
-  #[test]
-  fn section_attr_1() {
-    let content = "-- alfa: bravo";
-    let target1 = "alfa";
-    let target2 = "bravo";
-    let target3 = FlagOrAttr::SectionAttr {
-      key: target1.to_string(),
-      value: vec![Span::Text {
-        attributes: vec![],
-        content: target2.to_string(),
-        flags: vec![],
-        kind: "span".to_string(),
-        template: "default".to_string(),
-      }],
-    };
-    let input = Text::new_extra(content, "");
-    let result = section_attr(input).unwrap();
-    let left = target3;
-    let right = result.1;
-    assert_eq!(left, right,);
   }
 
   #[test]
--- src/span/single_newline.rs
+++ src/span/single_newline.rs
@@ -1,13 +1,25 @@
 use crate::Text;
 use nom::character::complete::{line_ending, space0};
-use nom::combinator::not;
+use nom::combinator::{eof, not, opt};
 use nom::{IResult, Parser};
+
+// REMINDER: If the single newline is at the end of the
+// file it returns an empty string instead of a space.
+// This includes if the only thing on the line below
+// it is whitespace before the end of file.
 
 pub fn single_newline(input: Text) -> IResult<Text, Text> {
   let (input, _) = space0.parse(input)?;
   let (input, _) = line_ending.parse(input)?;
-  let (input, _) = space0.parse(input)?;
-  let (input, _) = not(line_ending).parse(input)?;
+  if input.fragment().is_empty() {
+    return Ok((input, Text::new_extra("", "")));
+  }
+  let (_, check) = opt((space0, eof)).parse(input)?;
+  if check.is_some() {
+    return Ok((input, Text::new_extra("", "")));
+  }
+  let (input, _) =
+    not((space0, line_ending)).parse(input)?;
   Ok((input, Text::new_extra(" ", "")))
 }
 
@@ -18,9 +30,33 @@ mod tests {
   use rstest::rstest;
 
   #[rstest]
-  #[case("\n", " ", "")]
-  #[case("\nx", " ", "x")]
+  #[case(
+    "single newline at end of file returns empty string",
+    "\n",
+    "",
+    ""
+  )]
+  #[case(
+    "single newline with content after it returns space",
+    "\nx",
+    " ",
+    "x"
+  )]
+  #[case(
+    "single newline with spaces then content after it returns space and does not chomp whitespace on the next line",
+    "\n  x",
+    " ",
+    "  x"
+  )]
+  #[case(
+    "single newline followed by line with only whitespace that reaches end of file returns an empty string",
+    "\n ",
+    "",
+    " "
+  )]
+
   fn single_newline_test_runner(
+    #[case] description: &str,
     #[case] given: &str,
     #[case] expected: &str,
     #[case] remainder: &str,
@@ -30,22 +66,30 @@ mod tests {
     assert_eq!(
       &expected,
       result.1.fragment(),
-      "{}",
-      format!("\n\n{:?}\n\n{:?}", input, result)
+      "\n\n{}\n\n",
+      description
     );
     assert_eq!(
       &remainder,
       result.0.fragment(),
-      "{}",
-      format!("\n\n{:?}\n\n{:?}", input, result)
+      "\n\n{}\n\n",
+      description
     );
   }
 
-  #[test]
-  fn single_newline_error_test_runner() {
-    let input = Text::new_extra("\n\n", "");
+  #[rstest]
+  #[case("multiple newlines are an error", "\n\n")]
+  #[case(
+    "multiple followed by empty line with whitespace is an error",
+    "\n     \n"
+  )]
+  fn single_newline_error_test_runner(
+    #[case] description: &str,
+    #[case] given: &str,
+  ) {
+    let input = Text::new_extra(given, "");
     let result = single_newline.parse(input);
-    assert!(result.is_err());
+    assert!(result.is_err(), "\n\n{}\n\n", description);
   }
 
   //
