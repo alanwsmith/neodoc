781053a0bfc5e847ae1efd7fe2de75d9b16a076e
Alan Smith <commit@alanwsmith.com>
2026-08-05 10:09:36 -0400

    putting tests back in place after move to LocateSpan

--- src/span/whitespace0.rs
+++ src/span/whitespace0.rs
@@ -5,12 +5,16 @@ use nom::{IResult, Parser};
 // REMINDER: No spaces returns an empty string
 // One or more whitespace characters returns
 // a single space.
-pub fn whitespace0(input: Text) -> IResult<Text, Text> {
+//
+// REMINDER: This does not include line_endings
+//
+pub fn whitespace0(mut input: Text) -> IResult<Text, Text> {
+  input.extra = "whitespace0";
   let (input, result) = space0.parse(input)?;
   if result.is_empty() {
-    Ok((input, Text::new_extra("", "")))
+    Ok((input, Text::new_extra("", "whitespace0")))
   } else {
-    Ok((input, Text::new_extra("", "")))
+    Ok((input, Text::new_extra(" ", "whitespace0")))
   }
 }
 
@@ -19,6 +23,33 @@ mod tests {
   use super::*;
   use pretty_assertions::assert_eq;
   use rstest::rstest;
+
+  #[rstest]
+  #[case("", "", "")]
+  #[case(" ", " ", "")]
+  #[case("     ", " ", "")]
+  #[case("  \t   ", " ", "")]
+  #[case("     x", " ", "x")]
+  fn whitespace0_test_runner(
+    #[case] given: &str,
+    #[case] expected: &str,
+    #[case] remainder: &str,
+  ) {
+    let input = Text::new_extra(given, "");
+    let result = whitespace0.parse(input).unwrap();
+    assert_eq!(
+      &expected,
+      result.1.fragment(),
+      "{}",
+      format!("\n\n{:?}\n\n{:?}", input, result)
+    );
+    // assert_eq!(
+    //   &remainder,
+    //   result.0.fragment(),
+    //   "{}",
+    //   format!("\n\n{:?}\n\n{:?}", input, result)
+    // );
+  }
 
   // #[rstest]
   // #[case("", "", "")]
@@ -35,4 +66,6 @@ mod tests {
   //   let right = whitespace0(given).unwrap();
   //   assert_eq!(left, right);
   // }
+
+  //
 }
